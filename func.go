135a9329e38ec5571f86d73ba44bf116054940e1
Cuiks <1253016652@qq.com>
2021-02-22 17:51:34 +0800

    2020022202

--- func.go
+++ func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
 
 func eval(a, b int, op string) (int, error) {
 	switch op {
@@ -19,16 +24,54 @@ func eval(a, b int, op string) (int, error) {
 }
 
 func div(a, b int) (q, r int) {
-	return a / b, a % b
+	q, r = a/b, a%b
+	return
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %s with args "+"(%d, %d)\n", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+func sum(numbers ...int) int {
+	// 可变参数列表
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
+}
+
+func swap(a, b *int) {
+	// 值传递 引用传递
+	*a, *b = *b, *a
+	// return b, a
 }
 
 func main() {
 	if result, err := eval(3, 4, "x"); err != nil {
-		fmt.Println(err)
+		fmt.Println("Error:", err)
 	} else {
 		fmt.Println(result)
 	}
 
 	q, r := div(13, 3)
 	fmt.Println(q, r)
+
+	fmt.Println(apply(pow, 3, 4))
+	fmt.Println(apply(func(a int, b int) int {
+		return int(math.Pow(float64(a), float64(b)))
+	}, 3, 4))
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	a, b := 3, 4
+	swap(&a, &b)
+	fmt.Println(a, b)
 }
